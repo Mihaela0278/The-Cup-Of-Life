ea2fbf56f3552710735d5a59664a13f22bfe7c59
Mihaela Hristova <106870022+Mihaela0278@users.noreply.github.com>
2022-06-12 19:54:27 +0300

    Add TheCupOfLife.Data class library and install nuget packages

---
+++ TheCupOfLife.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TheCupOfLife.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ TheCupOfLife.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TheCupOfLife.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
